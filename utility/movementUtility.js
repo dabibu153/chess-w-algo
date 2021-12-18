8f0f21d6243a0256ff8db86954c17d4450ce20f4
saurav sharma <68466028+dabibu153@users.noreply.github.com>
2021-12-18 22:15:04 +0530

    pr test

--- utility/movementUtility.js
+++ utility/movementUtility.js
@@ -1,6 +1,8 @@
 const rows = [1, 2, 3, 4, 5, 6, 7, 8];
 const cols = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
+//pr test
+
 export const calculatePosibleMovementLocations = (
   current,
   activeSide,
